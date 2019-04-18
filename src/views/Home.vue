da1c614064a1efc85e7906045619a24066f1c9a9
Jon Glass <jonglass82@gmail.com>
2019-04-18 16:01:32 -0500

    add create route page and added css to listings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,71 +2,66 @@
   <div class="home">
 <h1>Listings</h1>
 
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#routeModal">
-  Create a route
-</button>
+<router-link to="/route">Create a route</router-link>
 
-<div class="modal fade" id="routeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Pick a date for your route:</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-
-      <datepicker :inline="true" v-model="myDate"></datepicker>
-        <h1>{{myDate}}</h1>
-        ...
-      </div>
-      <div class="modal-footer">
-
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
-        <button type="button" class="btn btn-primary" v-on:click="chooseListings()">Choose listings</button>
-
-      </div>
-    </div>
-  </div>
-</div>
+<div class="container" id="main">
 
   <div v-for="listing in listings">
-    <h1>{{listing.address}}</h1>
-    <h3>{{listing.description}}</h3>
+
+    <div class="container" id="listingContainer">
+    <h5>{{listing.address}}</h5>
+    <p>{{listing.description}}</p>
   </div>
+  </div>
+</div>
 
 </div>
 </template>
 
+
+<style type="text/css">
+
+#main{
+  width: 90%;
+  text-align: left;
+}
+#main div:nth-of-type(2n){
+    background-color: aliceblue;
+}
+
+#listingContainer{
+  padding: 20px;
+}
+
+</style>
 
 
 <script>
 
 import axios from "axios";
 import Datepicker from 'vuejs-datepicker';
+import moment from 'moment';
 
 export default {
+
   data: function () {
     return {
-      listings: [],
-      myDate: null
+      listings: []
     };
   },
+
     components: {
-    Datepicker
+
   },
+
   created: function() {
     axios.get("/listings").then(response => {
       this.listings = response.data.listings;
       console.log(this.listings);
     })
   },
-  methods: {
-    chooseListings: function() {
-      console.log("chooseListings!!!", this.myDate)
-    }
-  }
+
+  methods: {}
+
 };
 </script>
--- src/views/Route.vue
+++ src/views/Route.vue
@@ -1,40 +1,140 @@
 <template>
-  <div class="route">
-    <h1>Create a route</h1>
+  <div class="home">
+<h1>Listings</h1>
 
+<div id="routeText">
 
-  <h3>Listings</h3>
-
-  <div v-for="listing in listings">
-    <h2>{{listing.address}}</h2>
-    <h3>{{listing.description}}</h3>
-
-    <input type="checkbox" id="checkbox" v-model="checked">
-    <label for="checkbox">{{ checked }}</label>
-  </div>
+<h3>{{myDate}}</h3>
 
 </div>
 
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#routeModal">
+  Pick a date
+</button>
+
+<div class="modal fade" id="routeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Pick a date for your route:</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <datepicker :inline="true" v-model="myDate"></datepicker>
+
+        <h4>{{myDate}}</h4>
+  
+      </div>
+      <div class="modal-footer">
+
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+        <button type="button" class="btn btn-primary" v-model="myDate" data-dismiss="modal">Choose listings</button>
+
+   
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="main" class="container">
+  <div v-for="listing in listings" v-bind:key="listing.id">
+
+    <div class="container" id="listingContainer">
+    
+    <h5>{{listing.address}}</h5>
+    <p>{{listing.description}}</p>
+   <!--  <h4>{{listing.id}}</h4> -->
+    <button class="listingBtn" v-on:click="addToRoute()">Add to route</button>
+
+  </div>
+ 
+  </div>
+</div>
+
+</div>
 </template>
+
+<style type="text/css">
+
+#main{
+  width: 90%;
+  text-align: left;
+}
+#main div:nth-of-type(2n){
+    background-color: aliceblue;
+}
+
+#listingContainer{
+  padding: 20px;
+}
+#routeText{
+  padding: 10px;
+}
+
+</style>
+
 
 
 <script>
 
 import axios from "axios";
+import Datepicker from 'vuejs-datepicker';
+import moment from 'moment';
 
 export default {
+
   data: function () {
     return {
-      listings: []
+      listings: [],
+      route: [],
+      listing_id: "",
+      myDate: ""
     };
   },
+
+    components: {
+    Datepicker
+  },
+
   created: function() {
     axios.get("/listings").then(response => {
       this.listings = response.data.listings;
       console.log(this.listings);
     })
-    $('.modal-backdrop').remove();
   },
-  methods: {}
+
+  methods: {
+    createRoute: function () {
+      var params = {
+        date: this.myDate,
+      }
+      axios.post("/routes",params).then(response => {
+      this.route = response.data.route;
+    });
+
+    },
+    addToRoute: function () {
+
+      var params = {
+        listing_id: this.listing.id
+      }
+       console.log(params);
+
+      axios.post("/listings_routes",params).then(response => {
+      this.listing = response.data.listing;
+
+       
+
+    });
+
+    },
+    
+  }
+
 };
 </script>
